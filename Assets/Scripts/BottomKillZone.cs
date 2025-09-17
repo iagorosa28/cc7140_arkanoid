b1898a7ca1428464cd8e539764e976f7f6cd1f62
Iago Rosa <iagoimportantthings@gmail.com>
2025-09-17 19:57:01 -0300

    feat: Zona de queda

---
+++ Assets/Scripts/BottomKillZone.cs
@@ -0,0 +1,23 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BottomKillZone : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider other)
+    {
+        if(other.CompareTag("Ball"))
+        {
+            GameManagerArkanoid.LoseLife();
+
+            // reinicia a bola se ainda houver vidas
+            var ball = other.GetComponent<BallArkanoid>();
+            if(ball != null && GameManagerArkanoid.lives > 0)
+            {
+                ball.restartBall();
+            }
+        }
+    }
+
+}
